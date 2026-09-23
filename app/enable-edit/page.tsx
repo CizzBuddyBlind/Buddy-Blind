3c0c7b97a06ff5fe118afe39d46141218a466552
CizzBuddyBlind <cizz.j.c@gmail.com>
2026-09-23 22:13:41 +1200

    Create page.tsx

---
+++ app/enable-edit/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+import { useEffect } from 'react'
+export default function EnableEdit(){
+  useEffect(()=>{
+    localStorage.setItem('bb_force_edit','1')
+    localStorage.setItem('buddy_is_founder','1')
+    localStorage.setItem('buddy_email','founder@buddyblind.com')
+    localStorage.setItem('buddy_user', JSON.stringify({email:'founder@buddyblind.com'}))
+    localStorage.setItem('buddy_registered','1')
+    setTimeout(()=>{ window.location.href='/' }, 500)
+  },[])
+  return <div style={{background:'black',color:'white',padding:40}}>Enabling edit mode... redirecting to home...</div>
+}
