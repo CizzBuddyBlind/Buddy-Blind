3f3be5cd5b453e48f42aedd8dcaf8c5f5e2899b7
CizzBuddyBlind <cizz.j.c@gmail.com>
2026-09-23 22:41:40 +1200

    Update page.tsx

--- app/how-it-works/page.tsx
+++ app/how-it-works/page.tsx
@@ -2,11 +2,10 @@
 import { EditableText } from '@/components/EditableText'
 export default function HowItWorksPage(){
   return (
-    <div className="min-h-screen bg-black text-white pt-20 px-6">
+    <div className="min-h-screen bg-black text-white pt-24 px-6 pb-20">
       <div className="max-w-5xl mx-auto">
-        <EditableText textKey="hiw_big" defaultValue="See venue, see vibe, join." as="h1" className="text- leading-[0.9] font-serif" />
+        <EditableText textKey="hiw_big" defaultValue="See venue, see vibe, join." as="h1" className="text- leading-[0.9] font-serif block" />
         <EditableText textKey="hiw_sub" defaultValue="Three steps to a blind dinner." className="mt-6 text-zinc-400 block" />
-        {/* keep rest of your page, but wrap any text with EditableText */}
       </div>
     </div>
   )
