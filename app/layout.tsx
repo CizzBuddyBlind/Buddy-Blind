be8673c052451e9318f0620b25daba734889096e
CizzBuddyBlind <cizz.j.c@gmail.com>
2026-09-23 17:49:46 +1200

    Add files via upload

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,24 @@
 
 import './globals.css'
-import AdminEditor from "@/components/AdminEditor"
 import Navbar from '@/components/Navbar'
 import ColorProvider from '@/components/ColorProvider'
 import { AuthProvider } from '@/components/AuthContext'
+import AdminEditor from '@/components/AdminEditor'
+
 export const metadata={title:'BUDDY BLIND V9',description:'You dont know who you will meet'}
-export default function RootLayout(<AdminEditor>{children}</AdminEditor>:{children:React.ReactNode}){
+
+export default function RootLayout({children}:{children:React.ReactNode}){
   return(
     <html lang="en">
       <body className="bg-[#080808] antialiased">
-        <AuthProvider>
-          <ColorProvider>
-            <Navbar/><AdminEditor>{children}</AdminEditor>
-          </ColorProvider>
-        </AuthProvider>
+        <ColorProvider>
+          <AuthProvider>
+            <AdminEditor>
+              <Navbar />
+              {children}
+            </AdminEditor>
+          </AuthProvider>
+        </ColorProvider>
       </body>
     </html>
   )
